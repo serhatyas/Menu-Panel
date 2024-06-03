a7564f831406f1f7fcafb29fd76f67b1e1b10f70
Serhat Yaş <serhatyas15@gmail.com>
2024-06-03 13:59:06 +0300

    Fluent Validation

---
+++ NLayer.Service/Validations/ProductDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using NLayer.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Validations
+{
+    public class ProductDtoValidator:AbstractValidator<ProductDto>
+    {
+
+        public ProductDtoValidator()
+        {
+           RuleFor(x=>x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
+
+           RuleFor(X => X.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be grater 0");
+           RuleFor(X => X.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be grater 0");
+           RuleFor(X => X.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be grater 0");
+        }
+    }
+}
